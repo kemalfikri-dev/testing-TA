93a5917ba7a33bd68ae024d9dfb70fedc9c2ed3f
kemalfikri-dev <mkemal.fikrii@gmail.com>
2026-04-23 21:24:34 +0700

    fixing DB Connection

--- app.js
+++ app.js
@@ -45,7 +45,7 @@
   });
 
   // ── Jalankan Server ──
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
     console.log(`Server jalan di http://localhost:${PORT}`);
   });
